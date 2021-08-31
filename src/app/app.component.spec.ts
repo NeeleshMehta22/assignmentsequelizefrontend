52de972e53ee34661450b251a8159bb4a5022752
NeeleshMehta22 <neelesh.mehta@sourcefuse.com>
2021-08-31 12:32:25 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'usermanagement'`, () => {
+  it(`should have as title 'angularapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('usermanagement');
+    expect(app.title).toEqual('angularapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('usermanagement app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularapp app is running!');
   });
 });
